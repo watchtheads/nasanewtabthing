e210d34998c627e8585693ed44ef6aadadc7afee
watchtheads <229876127+AyaanJayara@users.noreply.github.com>
2026-07-22 10:49:44 +1000

    agfdgdfggic

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import './style.css';
 const API_KEY = import.meta.env.VITE_NASA_API_KEY;
 document.querySelector("#app").innerHTML = "<p>loading...</p>";
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
